bff53967cb790643e6d2200c4404f4351d9eb8b5
FusionTech <125404931+fusiontechofficial@users.noreply.github.com>
2024-05-03 18:15:54 +0530

    Add files via upload

---
+++ coin-warrior-sdk-master/src/constants.ts
@@ -0,0 +1,50 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 56,
+  TESTNET = 97
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = {
+  97: '0x8E79ab32b3EdEB011E81c45C41b3339B63db9eAa',
+  56: '0x517825e4fEa28a4db8EaD4e1f0FF1D5266ef2135'
+}
+
+export const INIT_CODE_HASH = '0xb16460eeb33d1d2003bc8df891a9ffbfb529eb27660e7802b025508663fe4ef7'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const FEES_NUMERATOR = JSBI.BigInt(9975)
+export const FEES_DENOMINATOR = JSBI.BigInt(10000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
