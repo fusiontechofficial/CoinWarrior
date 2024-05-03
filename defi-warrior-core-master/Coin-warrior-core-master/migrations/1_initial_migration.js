3642eeec8963ccc4bd47bbf2dcb5e2f6b09d7e5a
FusionTech <125404931+fusiontechofficial@users.noreply.github.com>
2024-05-03 18:00:25 +0530

    Delete defi-warrior-core-master directory

--- defi-warrior-core-master/Coin-warrior-core-master/migrations/1_initial_migration.js
+++
@@ -1,9 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-const CoinWarriorFactory = artifacts.require("CoinWarriorFactory");
-const NFTWarrior = artifacts.require("NFTWarrior");
-
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(CoinWarriorFactory, "0x6148Ce093DCbd629cFbC4203C18210567d186C66");
-  deployer.deploy(NFTWarrior, "Defi Warrior", "FIWA");
-};
